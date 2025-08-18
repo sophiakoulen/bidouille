5d703bd625720dce2abc11da145033d8f1a07260
Sophia Koulen <sophia.koulen@outlook.com>
2025-08-18 18:47:42 +0200

    succeeded.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 use std::env;
 use std::fs;
-use std::io;
-use std::io::Write;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -10,19 +8,35 @@ fn main() {
 
     let bytes = fs::read(&filename).unwrap();
 
-    let header = &bytes[..78];
+    let id1: &[u8;4] = &bytes[0..4].try_into().unwrap();
 
-    let old_size: &[u8;4] = &bytes[78..82].try_into().unwrap();
+    let file_size: &[u8;4] = &bytes[4..8].try_into().unwrap();
 
-    let data = &bytes[82..];
+    let id2: &[u8;4] = &bytes[8..12].try_into().unwrap();
+
+    let header = &bytes[12..74];
+
+    let old_size: &[u8;4] = &bytes[74..78].try_into().unwrap();
+
+    let data = &bytes[78..];
 
     let old_size = u32::from_le_bytes(*old_size);
+
+    dbg!(&old_size);
+
+    let file_size = u32::from_le_bytes(*file_size);
+
+    dbg!(&file_size);
+
+    let new_file_size: u32 = (file_size + old_size).try_into().unwrap();
+    let new_file_size = new_file_size.to_le_bytes();
+
     let new_size: u32 = (old_size * 2).try_into().unwrap();
     let new_size = new_size.to_le_bytes();
 
     dbg!(&new_size);
 
-    let truc = vec![header.to_vec(), new_size.to_vec(), data.to_vec()].concat();
+    let truc = vec![id1.to_vec(), new_file_size.to_vec(), id2.to_vec(), header.to_vec(), new_size.to_vec(), data.to_vec(), data.to_vec()].concat();
 
     fs::write("output.wav", &truc).unwrap();
 
