569e324892be7a2030a4c596b4d68930f25cc0aa
Sophia Koulen <sophia.koulen@outlook.com>
2025-08-23 17:55:26 +0200

    concat 2 different files.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::fs;
+use std::process::exit;
 
 struct WavFile{
     filesize: u32,
@@ -66,13 +67,29 @@ fn WavFile_to_bytes(file: &WavFile)->Vec<u8>
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let filename = &args[1];
+    if args.len() < 2 {
+        println!("Error: Should have 1 or 2 arguments.\n");
+        exit(2);
+    }
 
-    let bytes = fs::read(&filename).unwrap();
+    let file1 = &args[1];
+    let bytes1 = fs::read(&file1).unwrap();
+    let content1 = parse_file(bytes1).unwrap();
 
-    let content = parse_file(bytes).unwrap();
+    let result;
 
-    let result = concat_WavFile(&content, &content).unwrap();
+    if args.len() > 2
+    {
+        
+        let file2 = &args[2];
+        let bytes2 = fs::read(&file2).unwrap();
+        let content2 = parse_file(bytes2).unwrap();
+        result = concat_WavFile(&content1, &content2).unwrap();
+    }
+    else
+    {
+        result = concat_WavFile(&content1, &content1).unwrap();
+    }
 
     let output_bytes = WavFile_to_bytes(&result);
 
