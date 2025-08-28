593dfe6cb735a1bda17b1dabba945eb8c93e4b59
Sophia Koulen <sophia.koulen@outlook.com>
2025-08-28 20:03:24 +0200

    Error handling.

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,34 @@
 use std::env;
 use std::fs;
 use std::process::exit;
+use std::io::ErrorKind;
 
-struct WavFile{
+struct WavFile {
     filesize: u32,
     datasize: u32,
     header: Vec<u8>,
     data: Vec<u8>,
 }
 
-fn parse_file(bytes: Vec<u8>)->Option<WavFile>{
-    if &bytes[0..4] != b"RIFF"
-    {
-        println!("File format not recognized.");
-        return None;
+#[derive(Debug)]
+enum ParseError {
+    FileFormat,
+}
+
+fn parse_file(bytes: Vec<u8>)->Result<WavFile, ParseError> {
+    if &bytes[0..4] != b"RIFF" {
+        return Err(ParseError::FileFormat);
     }
     
-    if &bytes[8..12] != b"WAVE"
-    {
-        println!("File format not recognized.");
-        return None;
+    if &bytes[8..12] != b"WAVE" {
+        return Err(ParseError::FileFormat);
     }
 
     let filesize: &[u8;4] = &bytes[4..8].try_into().unwrap();
     let filesize = u32::from_le_bytes(*filesize);
     
-    if &bytes[70..74] != b"data"
-    {
-        println!("File format not recognized.");
-        return None;
+    if &bytes[70..74] != b"data" {
+        return Err(ParseError::FileFormat);
     }
 
     let header = bytes[12..70].to_vec();
@@ -38,22 +38,37 @@ fn parse_file(bytes: Vec<u8>)->Option<WavFile>{
 
     let data = bytes[78..].to_vec();
 
-    Some(WavFile {filesize, datasize, header, data})
+    Ok(WavFile {filesize, datasize, header, data})
 }
 
-fn concat_WavFile(a: &WavFile, b: &WavFile)->Option<WavFile>
-{
-    let filesize = a.filesize + b.datasize;
-    let datasize = a.datasize + b.datasize;
+#[derive(Debug)]
+enum OperationError {
+    ResultingFileTooLarge,
+}
+
+fn concat_WavFile(a: &WavFile, b: &WavFile)->Result<WavFile, OperationError> {
+    let filesize = a.filesize.checked_add(b.datasize);
+    let filesize = match filesize {
+        Some(a) => a,
+        None => {
+            return Err(OperationError::ResultingFileTooLarge);
+        }
+    };
+    let datasize = a.datasize.checked_add(b.datasize);
+    let datasize = match datasize {
+        Some(a) => a,
+        None => {
+            return Err(OperationError::ResultingFileTooLarge);
+        }
+    };
 
     let header = a.header.clone();
     let data = [a.data.to_vec(), b.data.to_vec()].concat();
 
-    Some(WavFile{filesize, datasize, header, data})
+    Ok(WavFile{filesize, datasize, header, data})
 }
 
-fn WavFile_to_bytes(file: &WavFile)->Vec<u8>
-{
+fn WavFile_to_bytes(file: &WavFile)->Vec<u8> {
     vec![
         b"RIFF".to_vec(),
         file.filesize.to_le_bytes().to_vec(),
@@ -64,6 +79,43 @@ fn WavFile_to_bytes(file: &WavFile)->Vec<u8>
     ].concat()
 }
 
+fn open_WavFile(filename: &str)->WavFile {
+    let res = fs::read(filename);
+    let bytes = match res{
+        Ok(bytes) => bytes,
+        Err(e) => {
+            if e.kind() == ErrorKind::NotFound {
+                println!("Error: could not find input file '{filename}'.");
+                exit(1);
+            }
+            else if e.kind() == ErrorKind::PermissionDenied {
+                println!("Error: permission denied for input file '{filename}'.");
+                exit(1);
+            }
+            else {
+                panic!("Failed to open input file '{filename}': {e:?}.");
+            }
+        }
+    };
+    let parse_result = parse_file(bytes);
+    let content = match parse_result {
+        Ok(content) => content,
+        Err(e) => {
+            match e {
+                ParseError::FileFormat => {
+                    println!("Failed to parse input file '{filename}': file format not recognised.");
+                    exit(1);
+                }
+                _ => {
+                    println!("Failed to parse input file '{filename}'.");
+                    exit(1);
+                }
+            }
+        }
+    };
+    content
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
@@ -72,26 +124,34 @@ fn main() {
         exit(2);
     }
 
-    let file1 = &args[1];
-    let bytes1 = fs::read(&file1).unwrap();
-    let content1 = parse_file(bytes1).unwrap();
-
+    let wav1 = open_WavFile(&args[1]);
     let result;
 
-    if args.len() > 2
-    {
-        
-        let file2 = &args[2];
-        let bytes2 = fs::read(&file2).unwrap();
-        let content2 = parse_file(bytes2).unwrap();
-        result = concat_WavFile(&content1, &content2).unwrap();
+    if args.len() > 2 {
+        let wav2 = open_WavFile(&args[2]);
+        result = concat_WavFile(&wav1, &wav2);
     }
-    else
-    {
-        result = concat_WavFile(&content1, &content1).unwrap();
+    else {
+        result = concat_WavFile(&wav1, &wav1);
     }
+
+    let result = match result {
+        Ok(res) => res,
+        Err(e) => {
+            match e {
+                OperationError::ResultingFileTooLarge => {
+                    println!("Failed to concatenate the input files because the resulting file would be too large.");
+                    exit(1);
+                },
+                _ => {
+                    println!("Failed to concatenate the input files.");
+                    exit(1);
+                }
+            };
+        }
+    };
 
     let output_bytes = WavFile_to_bytes(&result);
-
-    fs::write("output.wav", &output_bytes).unwrap();
+    let output_filename = "output.wav";
+    fs::write(output_filename, &output_bytes).expect("Failed to write to output file.");
 }
